cefa11d90a70fe5806a3a10c82c2ada384f71254
inji0212 <in17100170@kakao.com>
2024-02-14 11:34:56 +0900

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+#500,100,50,10
+#n원 최소 갯수
+
+n= int (input())
+coin=[500,100,50,10]
+count =0
+for i in coin :
+    if n>= i:
+        count+=n//i
+        n= n%i
+
+print(count)
+
+
+coins.sort(reverse=True)
